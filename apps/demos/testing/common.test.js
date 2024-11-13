641b451faad06dc2656c6dfd990d6ae7387f0080
Arpad Ivany <arpad.ivany@hes.dev>
2024-11-13 09:49:30 +0100

    reverted test skip

--- apps/demos/testing/common.test.js
+++ apps/demos/testing/common.test.js
@@ -45,7 +45,7 @@ const execTestCafeCode = (t, code) => {
   return testCafeFunction(t);
 };
 
-const COMMON_SKIP_RULES = [];
+const COMMON_SKIP_RULES = ['color-contrast'];
 const getTestSpecificSkipRules = (testName) => {
   switch (testName) {
     case 'Calendar-MultipleSelection':
