43f870b44f5c5b49df114cc16060abcc0f981b46
Strider2342 <arpad.ivany@gmail.com>
2024-12-06 12:25:37 +0100

    feat(chat): added screenshot tests for chat overview demo

---
+++ apps/demos/testing/widgets/chat/Overview.test.js
@@ -0,0 +1,28 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
+
+fixture('Chat.Overview')
+  .page('http://localhost:8080/')
+  .before(async (ctx) => {
+    ctx.initialWindowSize = [900, 800];
+  });
+
+runManualTest('Chat', 'Overview', ['jQuery'], (test) => {
+  test('Overview', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t.typeText('#user-chat .dx-texteditor-input', 'testing');
+
+    await testScreenshot(t, takeScreenshot, 'chat_typing_indicator_appears_in_user_chat.png');
+
+    await t.typeText('#support-chat .dx-texteditor-input', 'testing');
+
+    await testScreenshot(t, takeScreenshot, 'chat_typing_indicator_appears_in_support_chat.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
