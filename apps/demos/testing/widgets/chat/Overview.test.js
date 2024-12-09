1463926eb05c4a924e8beae78222df47cf7d5f67
ksercs <ksercs0@gmail.com>
2024-12-10 01:17:47 +0400

    simplify test

--- apps/demos/testing/widgets/chat/Overview.test.js
+++ apps/demos/testing/widgets/chat/Overview.test.js
@@ -1,5 +1,5 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { ClientFunction, Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
 
@@ -14,21 +14,15 @@ fixture('Chat.Overview')
 runManualTest('Chat', 'Overview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
   test('Overview', async (t) => {
     await ClientFunction(() => {
-      // const styleElement = document.createElement('style');
-      // styleElement.innerHTML = `.dx-chat-typingindicator-circle { animation: none !important; }`;  
-      // document.head.appendChild(styleElement);
+      const styleElement = document.createElement('style');
+      styleElement.innerHTML = `.dx-chat-typingindicator-circle { animation: none !important; }`;  
+      document.head.appendChild(styleElement);
     });
 
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-    const leftChat = Selector(CHAT_INPUT_SELECTOR).nth(0);
-    const rightChat = Selector(CHAT_INPUT_SELECTOR).nth(1);
     
-    await t.typeText(leftChat, 'left');
-    await testScreenshot(t, takeScreenshot, 'chat_typing_indicator_appears_in_user_chat.png');
-
-    await t.typeText(rightChat, 'right');
-    await testScreenshot(t, takeScreenshot, 'chat_typing_indicator_appears_in_support_chat.png');
+    await t.typeText(CHAT_INPUT_SELECTOR, 'left');
+    await testScreenshot(t, takeScreenshot, 'chat_typing_indicator.png');
 
     await t
       .expect(compareResults.isValid())
