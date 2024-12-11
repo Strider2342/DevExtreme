2d1e909e2cc9faf0709236f4ed33c4fdb1ffb50c
ksercs <ksercs0@gmail.com>
2024-12-11 19:36:30 +0400

    try test with old approached

--- apps/demos/testing/widgets/chat/Overview.test.js
+++ apps/demos/testing/widgets/chat/Overview.test.js
@@ -1,9 +1,9 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { ClientFunction } from 'testcafe';
+// import { ClientFunction } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
 
-const USER_CHAT_INPUT_SELECTOR = '#user-chat .dx-texteditor-input';
+// const USER_CHAT_INPUT_SELECTOR = '#user-chat .dx-texteditor-input';
 
 fixture('Chat.Overview')
   .page('http://localhost:8080/')
@@ -13,15 +13,15 @@ fixture('Chat.Overview')
 
 runManualTest('Chat', 'Overview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
   test('Overview', async (t) => {
-    await ClientFunction(() => {
-      const styleElement = document.createElement('style');
-      styleElement.innerHTML = '.dx-chat-typingindicator-circle { animation: none !important; }';
-      document.head.appendChild(styleElement);
-    })();
+    // await ClientFunction(() => {
+    //   const styleElement = document.createElement('style');
+    //   styleElement.innerHTML = '.dx-chat-typingindicator-circle { animation: none !important; }';
+    //   document.head.appendChild(styleElement);
+    // })();
 
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t.typeText(USER_CHAT_INPUT_SELECTOR, 'testing');
+    await t.typeText('#user-chat .dx-texteditor-input', 'testing');
 
     await testScreenshot(t, takeScreenshot, 'chat_typing_indicator.png');
 
